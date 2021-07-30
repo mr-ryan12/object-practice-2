701e617572858df1b7cfede2f4781ffc5a77953a
Ryan McBride <mr.ryan444@gmail.com>
2021-07-29 22:32:26 -0600

    Add car object

---
+++ object-examples.js
@@ -0,0 +1,11 @@
+var car = {
+    make: 'Lincoln',
+    model: 'Mark VII',
+    year: 1986,
+    miles: 246000,
+    isRunning: false,
+    needsRepairs: true,
+    issues: ['window motor broken', 'shocks busted', 'everything else that could go wrong'],
+    placesDriven: ['Dallas', 'Austin', 'Rockwall', 'Heath', 'Rowlett']
+};
+
